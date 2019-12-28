636d3671959f4bdf45d50e4cf29a5668066e567c
Wu Junkai <50454344+WuJunkai2004@users.noreply.github.com>
2019-12-28 17:50:38 +0800

    Update net.py

--- my_net/net.py
+++ my_net/net.py
@@ -1,1 +1,23 @@
-import
+# !bin/python
+# coding=utf-8
+
+from __future__ import print_function
+
+import urllib2
+import urllib
+import time
+import os
+import re
+
+class net(object):
+    def __init__(self,url,**kw):
+        self.url=url
+        ''''''
+        self._create()
+        try:
+            self._load()
+        except:
+            self._visit()
+            self._save()
+        else:
+            if(
