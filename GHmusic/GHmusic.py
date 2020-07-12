e8ffa01cfa9607ab9ce061bf50008170dc6b037b
Wu Junkai <50454344+WuJunkai2004@users.noreply.github.com>
2020-07-12 12:47:53 +0800

    Update GHmusic.py

--- GHmusic/GHmusic.py
+++ GHmusic/GHmusic.py
@@ -62,7 +62,6 @@ class GHmusic(object):
             "authority":"music.ghpym.com",
             "scheme":"https",
             "path":"/",
-            "cache-control":"max-age=0",
             "upgrade-insecure-requests":"1",
             "user-agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.97 Safari/537.36 Edg/83.0.478.45",
             "accept":"text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
@@ -170,7 +169,6 @@ class music(_info):
                 size=320
 
             head={
-                "Cache-Control":"max-age=0",
                 "Upgrade-Insecure-Requests":"1",
                 "User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.106 Safari/537.36 Edg/83.0.478.54",
                 "Accept":"text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
