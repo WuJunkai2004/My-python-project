a8eb0a73c36dc65f4ffa5ca043b43b3a86e1c8c9
Wu Junkai <50454344+WuJunkai2004@users.noreply.github.com>
2020-09-11 19:23:41 +0800

    Update wenkos.py

--- wenkos/wenkos.py
+++ wenkos/wenkos.py
@@ -1,1 +1,20 @@
+# coding=utf-8
 
+# * A SDK for wenku8.net ( no copyright )
+# * Wu Junkai wrote by python 3.4.4 , run in python 3,7,7
+
+__version__='0.00.1'
+__all__=[]
+
+try:
+    from urllib2   import urlopen
+    from urllib2   import Request
+    from urllib2   import build_opener
+    from urllib2   import HTTPCookieProcessor
+    from cookielib import CookieJar
+except ImportError:
+    from urllib.request import urlopen
+    from urllib.request import Request
+    from urllib.request import build_opener
+    from urllib.request import HTTPCookieProcessor
+    from http.cookiejar import CookieJar
